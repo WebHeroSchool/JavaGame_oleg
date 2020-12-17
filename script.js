eda15d9da294fda1b494af3c3bc8dbb69e8a303c
Oleg <>
2020-12-17 16:51:26 +0300

    Add trans

--- script.js
+++ script.js
@@ -2,29 +2,29 @@
 // выбор сложности, что передал параметр
 let level = 0;
 
-function f1(z) {
-  var m1 = document.getElementById('ff1');
-  var m2 = document.getElementById('ff2');
-  var m3 = document.getElementById('ff3');
+function selectLevel(z) {
+  let selectLevel1 = document.getElementById('selectLevel1');
+  let selectLevel2 = document.getElementById('selectLevel2');
+  let selectLevel3 = document.getElementById('selectLevel3');
 
   if (z == 1) {
-    m1.className = ('select__menu');
-    m2.classList.remove('select__menu');
-    m3.classList.remove('select__menu');
+    selectLevel1.className = ('select__menu');
+    selectLevel2.classList.remove('select__menu');
+    selectLevel3.classList.remove('select__menu');
     level = 3;
   }
 
   else if (z == 2) {
-    m2.className = ('select__menu');
-    m1.classList.remove('select__menu');
-    m3.classList.remove('select__menu');
+    selectLevel2.className = ('select__menu');
+    selectLevel1.classList.remove('select__menu');
+    selectLevel3.classList.remove('select__menu');
     level = 6;
   }
 
   else if (z == 3) {
-    m3.className = ('select__menu');
-    m1.classList.remove('select__menu');
-    m2.classList.remove('select__menu');
+    selectLevel3.className = ('select__menu');
+    selectLevel1.classList.remove('select__menu');
+    selectLevel2.classList.remove('select__menu');
     level = 10;
   }
 }
@@ -36,12 +36,11 @@ button.onclick = () => {
   else { start (); }
 };
 
-
 //добовление карт
 function start() {
-  var clean = document.getElementById('cleanTable');
+  let clean = document.getElementById('cleanTable');
   clean.className = ('start');
-  var hard = document.getElementById('ggame');
+  let hard = document.getElementById('ggame');
   if (level == 10) {
     hard.style.width = '1400px';
     hard.style.margin.left = '1400px';
@@ -49,7 +48,6 @@ function start() {
   else {
     hard.style.width = '800px';
   }
-
 
 //рандом не повторяемых цифир
   function rund(e, f) {
@@ -81,19 +79,16 @@ function start() {
 
 //цикл на кол-во карт
     for (i = 0; i < level; i++) {
-    var element = document.getElementById('game_js');
-    var div1 = document.createElement('div');
-    var num = z();
+    let element = document.getElementById('game_js');
+    let div1 = document.createElement('div');
+    let num = z();
     div1.classList.toggle('card');
-    div1.innerHTML = '<div class="card-inner" id="inner"><div class="card-front"><img src="img/card.png" /></div><div class="card-back"><img onclick="setInterval(() => {location.reload()}, 500);" src="img/card'+num+'.png" /></div></div>';
+    div1.innerHTML = '<div class="card-inner"  id="inner"><div class="card-front"><img src="img/card.png" /></div><div class="card-back"><img onclick="setInterval(() => {location.reload()}, 500);" src="img/card'+num+'.png" /></div></div>';
     element.appendChild(div1);
 }
 
 //обработка и переворот карты
+  let onclikNum = 0;
   let imgAll = document.querySelectorAll('.card-inner');
-  imgAll.forEach(function(img){
-  img.onclick = function(){
-    img.classList.toggle('trans')
-  }
-})
+  imgAll.forEach(function(img){ img.onclick = function(){ if (onclikNum == 0) {img.classList.toggle('trans'); onclikNum++} else {img.onclock = null;}}})
 }
